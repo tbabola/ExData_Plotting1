ba36a13d0806759bccb49989d2def1c18e1d6ae3
Travis B <tbabola@gmail.com>
2019-07-13 16:42:59 -0400

    Fixed labeling errors

--- plot3.R
+++ plot3.R
@@ -2,7 +2,7 @@
 # plot3 -------------------------------------------------------------------
 
 png(filename = "figure/plot3.png", width = 480, height = 480)
-plot(data$datetime,data$Sub_metering_1,type='l',ylab="Energy sub metering")
+plot(data$datetime,data$Sub_metering_1,type='l',ylab="Energy sub metering", xlab ="")
 lines(data$datetime,data$Sub_metering_2,type='l',col="red")
 lines(data$datetime,data$Sub_metering_3,type='l',col="blue")
 legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
--- plot4.R
+++ plot4.R
@@ -5,15 +5,15 @@ par(mfrow=c(2,2))
 plot(data$datetime,data$Global_active_power,type="l",xlab="",
      ylab="Global Active Power")
 
-plot(data$datetime,data$Voltage,type="l",
+plot(data$datetime,data$Voltage,type="l", xlab="datetime",
      ylab="Voltage")
 
-plot(data$datetime,data$Sub_metering_1,type='l',ylab="Energy sub metering")
+plot(data$datetime,data$Sub_metering_1,type='l',ylab="Energy sub metering", xlab="")
 lines(data$datetime,data$Sub_metering_2,type='l',col="red")
 lines(data$datetime,data$Sub_metering_3,type='l',col="blue")
 legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
        col=c("black","red","blue"),lty=1)
 
 plot(data$datetime,data$Global_reactive_power,type="l",
-     ylab="Global_reactive_power")
+     ylab="Global_reactive_power",xlab="datetime")
 dev.off()
